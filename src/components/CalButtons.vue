1a22c9e1bd738df01a4b2109d5f9082fd566f95b
KunhoLee <gunho1020@gmail.com>
2017-12-04 15:25:37 +0900

    App.vue에서 필요한 CalButtons.vue, CalDisplay.vue 컴포넌트 작성

---
+++ src/components/CalButtons.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="cal-buttons">
+    {{ testmsg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CalButtons',
+  data() {
+    return {
+      testmsg: 'CalButtons component',
+    };
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/CalDisplay.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="cal-display">
+    {{ testmsg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CalDisplay',
+  data() {
+    return {
+      testmsg: 'CalDisplay component',
+    };
+  },
+};
+</script>
+
+<style>
+</style>
