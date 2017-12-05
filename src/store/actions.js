8eae088ea1e956ca82ced0b2c0cd37725ba5c4ea
KunhoLee <gunho1020@gmail.com>
2017-12-05 11:58:49 +0900

    Vuex actions
    Constant.CLICK_CAL_BUTTON action에서 같은 이름의 mutation 호출, 전달받은 payload 그대로
    전달

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,1 +1,7 @@
-export default {};
+import Constant from '../Constant';
+
+export default {
+  [Constant.CLICK_CAL_BUTTON]: (store, payload) => {
+    store.commit(Constant.CLICK_CAL_BUTTON, payload);
+  },
+};
