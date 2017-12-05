14100ee9e54f823521f790045a81572b3f27cc43
KunhoLee <gunho1020@gmail.com>
2017-12-05 11:04:38 +0900

    CalButtons component의 buttonContents data를 Vuex의 state에서 관리하도록 변경

--- src/store/state.js
+++ src/store/state.js
@@ -1,3 +1,20 @@
 export default {
-  
+  buttonContents: [
+    { id: 7, content: '7' },
+    { id: 8, content: '8' },
+    { id: 9, content: '9' },
+    { id: 13, content: '÷' },
+    { id: 4, content: '4' },
+    { id: 5, content: '5' },
+    { id: 6, content: '6' },
+    { id: 12, content: '×' },
+    { id: 1, content: '1' },
+    { id: 2, content: '2' },
+    { id: 3, content: '3' },
+    { id: 11, content: '-' },
+    { id: 15, content: 'C' },
+    { id: 0, content: '0' },
+    { id: 14, content: '=' },
+    { id: 10, content: '+' },
+  ],
 };
