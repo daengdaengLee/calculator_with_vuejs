6c3b0ad83273ba187d434ad59e99d3ca87e4c9a0
KunhoLee <gunho1020@gmail.com>
2017-12-05 15:02:32 +0900

    Vuex mutations.js 불필요한 mutation 삭제, 필요한 mutation 추가
    Constant.CLICK_CAL_BUTTON 삭제
    Constant.CLICK_NUM_BUTTON 추가
    Constant.CLICK_OPERATOR_BUTTON 추가
    Contant.CLICK_EQUAL_BUTTON 추가
    Constant.CLICK_CLEAR_BUTTON 추가

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,8 +1,37 @@
 import Constant from '../Constant';
 
 export default {
-  [Constant.CLICK_CAL_BUTTON]: (state, payload) => {
+  [Constant.CLICK_NUM_BUTTON]: (state, payload) => {
     const storeState = state;
-    storeState.testmsg = String(payload.buttonId) + payload.buttonContent;
+    switch (storeState.currentInputType) {
+      case 'init':
+        storeState.calArray.push(payload.content);
+        break;
+      case 'number': {
+        let temp = storeState.calArray[storeState.calArray.length - 1];
+        temp += payload.content;
+        storeState.calArray.splice(storeState.calArray.length - 1, 1, temp);
+        break;
+      }
+      case 'operator':
+        storeState.calArray.push(payload.content);
+        break;
+      default:
+    }
+    storeState.currentInputType = 'number';
+  },
+  [Constant.CLICK_OPERATOR_BUTTON]: (state, payload) => {
+    const storeState = state;
+    storeState.calArray.push(payload.content);
+    storeState.currentInputType = 'operator';
+  },
+  [Constant.CLICK_EQUAL_BUTTON]: (state) => {
+    const storeState = state;
+    storeState.currentInputType = 'equal';
+  },
+  [Constant.CLICK_CLEAR_BUTTON]: (state) => {
+    const storeState = state;
+    storeState.calArray.splice(0);
+    storeState.currentInputType = 'init';
   },
 };
