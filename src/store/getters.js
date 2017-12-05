4bb5a000448fdfa022f62e19e096a2051d9e4c80
KunhoLee <gunho1020@gmail.com>
2017-12-05 16:23:10 +0900

    Vuex getters.js calDisplayNum
    각각의 상태에 따라 화면에 어떤 결과를 출력할지 결정

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,5 +1,28 @@
 export default {
   calDisplayNum(state) {
-    return state.testmsg;
+    let result;
+    switch (state.currentInputType) {
+      case 'init':
+        result = 0;
+        break;
+      case 'number': case 'operator':
+        result = state.calArray[state.calArray.length - 1].content;
+        break;
+      default:
+        for (let i = 0, len = state.calArray.length; i < len; i += 1) {
+          if (i === 0) {
+            result = Number(state.calArray[i].content);
+          } else if (state.calArray[i].type === 'plus') {
+            result += Number(state.calArray[i + 1].content);
+          } else if (state.calArray[i].type === 'minus') {
+            result -= Number(state.calArray[i + 1].content);
+          } else if (state.calArray[i].type === 'multiplication') {
+            result *= Number(state.calArray[i + 1].content);
+          } else if (state.calArray[i].type === 'division') {
+            result /= Number(state.calArray[i + 1].content);
+          }
+        }
+    }
+    return result;
   },
 };
