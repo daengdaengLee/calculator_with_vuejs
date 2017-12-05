67cef0154b1106580ee35714f5f6c87d10577170
KunhoLee <gunho1020@gmail.com>
2017-12-05 09:49:08 +0900

    CalButton component 생성, CalButtons component에 등록

---
+++ src/components/CalButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="cal-button">
+    {{ testmsg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CalButton',
+  data() {
+    return { testmsg: 'CalButton component' };
+  },
+};
+</script>
+
+<style>
+</style>
--- src/components/CalButtons.vue
+++ src/components/CalButtons.vue
@@ -1,16 +1,22 @@
 <template>
   <div id="cal-buttons">
     {{ testmsg }}
+    <cal-button></cal-button>
   </div>
 </template>
 
 <script>
+import CalButton from './CalButton';
+
 export default {
   name: 'CalButtons',
   data() {
     return {
       testmsg: 'CalButtons component',
     };
+  },
+  components: {
+    CalButton,
   },
 };
 </script>
