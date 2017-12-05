f0549d56a32cb8ae5404daa14e1589ee6c2eaa4d
KunhoLee <gunho1020@gmail.com>
2017-12-05 15:03:31 +0900

    Vuex actions.js Constant.CLICK_CAL_BUTTON action 변경
    어떤 종류의 버튼을 누르는지에 따라 각기 다른 mutation 호출

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,6 +2,37 @@ import Constant from '../Constant';
 
 export default {
   [Constant.CLICK_CAL_BUTTON]: (store, payload) => {
-    store.commit(Constant.CLICK_CAL_BUTTON, payload);
+    // payload = { buttonId: Number, buttonContent: String }
+    let type;
+    switch (payload.buttonId) {
+      case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7: case 8: case 9:
+        type = 'number';
+        break;
+      case 10: case 11: case 12: case 13:
+        type = 'operator';
+        break;
+      case 14:
+        type = 'equal';
+        break;
+      case 15:
+        type = 'clear';
+        break;
+      default:
+    }
+    switch (type) {
+      case 'number':
+        store.commit(Constant.CLICK_NUM_BUTTON, { content: payload.buttonContent });
+        break;
+      case 'operator':
+        store.commit(Constant.CLICK_OPERATOR_BUTTON, { content: payload.buttonContent });
+        break;
+      case 'equal':
+        store.commit(Constant.CLICK_EQUAL_BUTTON);
+        break;
+      case 'clear':
+        store.commit(Constant.CLICK_CLEAR_BUTTON);
+        break;
+      default:
+    }
   },
 };
