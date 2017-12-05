0841eed680cdd37762db60b4c6dcaef0ceec2499
KunhoLee <gunho1020@gmail.com>
2017-12-05 14:59:40 +0900

    Constant.js 필요한 상수 추가
    CLICK_NUM_BUTTON, CLICK_OPERATOR_BUTTON, CLICK_EQUAL_BUTTON,
    CLICK_CLEAR_BUTTON

--- src/Constant.js
+++ src/Constant.js
@@ -1,3 +1,7 @@
 export default {
   CLICK_CAL_BUTTON: 'clickCalButton',
+  CLICK_NUM_BUTTON: 'clickNumButton',
+  CLICK_OPERATOR_BUTTON: 'clickOperatorButton',
+  CLICK_EQUAL_BUTTON: 'clickEqualButton',
+  CLICK_CLEAR_BUTTON: 'clickClearButton',
 };
