511dddae20c744c2e7efc43c2ba1b207bf836e7b
KunhoLee <gunho1020@gmail.com>
2017-12-06 16:44:58 +0900

    calculator compoenent 내부로 cal-display, cal-buttons components 이동

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="calculator">
-    <p>test</p>
+    <cal-display></cal-display>
+    <cal-buttons></cal-buttons>
   </div>
 </template>
 
 <script>
+import CalDisplay from './CalDisplay';
+import CalButtons from './CalButtons';
+
 export default {
+  name: 'Calculator',
+  components: {
+    CalDisplay,
+    CalButtons,
+  },
 };
 </script>
 
