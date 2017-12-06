d8294e547788ff4513253a49d68178057a523f1b
KunhoLee <gunho1020@gmail.com>
2017-12-06 14:25:16 +0900

    release v1.0 으로 복원

--- src/Constant.js
+++ src/Constant.js
@@ -1,23 +1,7 @@
-export default function (type) {
-  let result;
-  switch (type) {
-    case 'CLICK_CAL_BUTTON':
-      result = 'clickCalButton';
-      break;
-    case 'CLICK_NUM_BUTTON':
-      result = 'clickNumButton';
-      break;
-    case 'CLICK_OPERATOR_BUTTON':
-      result = 'clickOperatorButton';
-      break;
-    case 'CLICK_EQUAL_BUTTON':
-      result = 'clickEqualButton';
-      break;
-    case 'CLICK_CLEAR_BUTTON':
-      result = 'clickClearButton';
-      break;
-    default:
-      result = 'No Constant';
-  }
-  return result;
-}
+export default {
+  CLICK_CAL_BUTTON: 'clickCalButton',
+  CLICK_NUM_BUTTON: 'clickNumButton',
+  CLICK_OPERATOR_BUTTON: 'clickOperatorButton',
+  CLICK_EQUAL_BUTTON: 'clickEqualButton',
+  CLICK_CLEAR_BUTTON: 'clickClearButton',
+};
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,7 +1,7 @@
 import Constant from '../Constant';
 
 export default {
-  [Constant('CLICK_NUM_BUTTON')]: (state, payload) => {
+  [Constant.CLICK_NUM_BUTTON]: (state, payload) => {
     const storeState = state;
     switch (storeState.currentInputType) {
       case 'init':
@@ -20,16 +20,16 @@ export default {
     }
     storeState.currentInputType = 'number';
   },
-  [Constant('CLICK_OPERATOR_BUTTON')]: (state, payload) => {
+  [Constant.CLICK_OPERATOR_BUTTON]: (state, payload) => {
     const storeState = state;
     storeState.calArray.push({ type: payload.type, content: payload.content });
     storeState.currentInputType = 'operator';
   },
-  [Constant('CLICK_EQUAL_BUTTON')]: (state) => {
+  [Constant.CLICK_EQUAL_BUTTON]: (state) => {
     const storeState = state;
     storeState.currentInputType = 'equal';
   },
-  [Constant('CLICK_CLEAR_BUTTON')]: (state) => {
+  [Constant.CLICK_CLEAR_BUTTON]: (state) => {
     const storeState = state;
     storeState.calArray.splice(0);
     storeState.currentInputType = 'init';
