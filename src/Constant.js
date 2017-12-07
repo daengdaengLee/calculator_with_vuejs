23b3e131db45fbc2e8d2cc97572de52a8ed2e49d
KunhoLee <gunho1020@gmail.com>
2017-12-07 21:09:44 +0900

    변경된 키 배열에 맞춰서 Constant와 actions 조정

--- src/Constant.js
+++ src/Constant.js
@@ -7,14 +7,20 @@ export default function (type) {
     case 'CLICK_NUM_BUTTON':
       result = 'clickNumButton';
       break;
+    case 'CLICK_DOT_BUTTON':
+      result = 'clickDotButton';
+      break;
     case 'CLICK_OPERATOR_BUTTON':
       result = 'clickOperatorButton';
       break;
     case 'CLICK_EQUAL_BUTTON':
       result = 'clickEqualButton';
       break;
-    case 'CLICK_CLEAR_BUTTON':
-      result = 'clickClearButton';
+    case 'CLICK_AC_BUTTON':
+      result = 'clickACButton';
+      break;
+    case 'CLICK_CE_BUTTON':
+      result = 'clickCEButton';
       break;
     default:
       result = 'No Constant';
