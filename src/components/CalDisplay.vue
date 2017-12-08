5a33fabb8aea63f2919bd60342753a4a48ecc942
KunhoLee <gunho1020@gmail.com>
2017-12-08 13:30:51 +0900

    디자인 변경 작업중

--- src/components/CalDisplay.vue
+++ src/components/CalDisplay.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="CalDisplay">
-    <p v-if="error">ERROR</p>
-    <p v-if="equalButton">{{ calResult }}</p>
-    <p v-if="!error && !equalButton">{{ calDisplayNum }}</p>
-    <p>{{ calDisplayExpression }}</p>
+    <p v-if="error" class="MainDisplay">ERROR</p>
+    <p v-if="equalButton" class="MainDisplay">{{ calResult }}</p>
+    <p v-if="!error && !equalButton" class="MainDisplay">{{ calDisplayNum }}</p>
+    <p class="SubDisplay">{{ calDisplayExpression }}</p>
   </div>
 </template>
 
@@ -21,4 +21,20 @@ export default {
 </script>
 
 <style scoped>
+.CalDisplay {
+  box-sizing: border-box;
+  border: 1px solid black;
+  width: 230px;
+  padding: 10px;
+}
+.CalDisplay .MainDisplay {
+  font-size: 1.2rem;
+  text-align: right;
+}
+.CalDisplay .SubDisplay {
+  font-size: 0.8rem;
+  color: gray;
+  text-align: right;
+  margin: 3px 0 0 0;
+}
 </style>
